23549d04f7ac4c2fbbfbfce4a9fa87c597f81e49
saengmotmi <ohjtack@gmail.com>
2023-12-07 02:39:47 +0900

    refactor: scanner 코드 정리

--- src/scanner.ts
+++ src/scanner.ts
@@ -13,168 +13,122 @@ const CharType = {
 type CharType = (typeof CharType)[keyof typeof CharType];
 
 const isCharType = (char: string, type: CharType) => {
+  const charCode = char.charCodeAt(0);
   switch (type) {
     case CharType.NumberLiteral:
-      return char >= "0" && char <= "9";
+      return charCode >= 48 && charCode <= 57; // '0' - '9'
     case CharType.StringLiteral:
-      return 32 <= char.charCodeAt(0) && char.charCodeAt(0) <= 126;
+      return charCode >= 32 && charCode <= 126; // Printable ASCII
     case CharType.IdentifierAndKeyword:
       return (
-        (char >= "a" && char <= "z") ||
-        (char >= "A" && char <= "Z") ||
-        (char >= "0" && char <= "9") ||
+        (charCode >= 97 && charCode <= 122) || // 'a' - 'z'
+        (charCode >= 65 && charCode <= 90) || // 'A' - 'Z'
+        (charCode >= 48 && charCode <= 57) || // '0' - '9'
         char === "_"
       );
     case CharType.OperatorAndPunctuator:
-      return (
-        char === "+" ||
-        char === "-" ||
-        char === "*" ||
-        char === "/" ||
-        char === "%" ||
-        char === "=" ||
-        char === "!" ||
-        char === "<" ||
-        char === ">" ||
-        char === "&" ||
-        char === "|" ||
-        char === "^" ||
-        char === "~" ||
-        char === "?" ||
-        char === ":" ||
-        char === "." ||
-        char === "," ||
-        char === ";" ||
-        char === "(" ||
-        char === ")" ||
-        char === "[" ||
-        char === "]" ||
-        char === "{" ||
-        char === "}"
-      );
+      return [
+        "+",
+        "-",
+        "*",
+        "/",
+        "%",
+        "=",
+        "!",
+        "<",
+        ">",
+        "&",
+        "|",
+        "^",
+        "~",
+        "?",
+        ":",
+        ".",
+        ",",
+        ";",
+        "(",
+        ")",
+        "[",
+        "]",
+        "{",
+        "}",
+      ].includes(char);
     default:
       return false;
   }
 };
 
-const scanNumberLiteral = (): Token => {
+const accumulateStringWhileCondition = (
+  condition: (char: string) => boolean
+): string => {
   let result = "";
-  // TODO: 실수(real number)에 대한 처리
-  while (isCharType(tokenIterResult.value, CharType.NumberLiteral)) {
+  while (!tokenIterResult.done && condition(tokenIterResult.value)) {
     result += tokenIterResult.value;
     tokenIterResult = tokenIter.next();
   }
-  return new Token(Kind.NumberLiteral, result);
+  return result;
+};
+
+const scanNumberLiteral = (): Token => {
+  const number = accumulateStringWhileCondition((char) =>
+    isCharType(char, CharType.NumberLiteral)
+  );
+  return new Token(Kind.NumberLiteral, number);
 };
 
 const scanStringLiteral = (): Token => {
   let result = "";
 
-  while (isCharType(tokenIterResult.value, CharType.StringLiteral)) {
-    if (tokenIterResult.value === '"' || tokenIterResult.value === "'") {
-      tokenIterResult = tokenIter.next(); // skip quote
-    }
+  // 첫 번째 따옴표 건너뛰기
+  tokenIterResult = tokenIter.next();
+
+  while (
+    !tokenIterResult.done &&
+    tokenIterResult.value !== '"' &&
+    tokenIterResult.value !== "'"
+  ) {
     result += tokenIterResult.value;
     tokenIterResult = tokenIter.next();
   }
+
+  // 마지막 따옴표 건너뛰기
+  tokenIterResult = tokenIter.next();
+
   return new Token(Kind.StringLiteral, result);
 };
 
 const scanIdentifierAndKeyword = (): Token => {
-  let result = "";
-
-  while (isCharType(tokenIterResult.value, CharType.IdentifierAndKeyword)) {
-    result += tokenIterResult.value;
-    tokenIterResult = tokenIter.next();
-  }
-
-  const kind = toKind(result);
-  if (kind === Kind.Unknown) {
-    return new Token(Kind.Identifier, result);
-  }
-  return new Token(kind);
+  const identifier = accumulateStringWhileCondition((char) =>
+    isCharType(char, CharType.IdentifierAndKeyword)
+  );
+  const kind = toKind(identifier);
+  return new Token(kind === Kind.Unknown ? Kind.Identifier : kind, identifier);
 };
 
 const scanOperatorAndPunctuator = (): Token => {
-  let string = "";
-  let currentChar = tokenIterResult.value;
-
-  // 연산자와 구두점 문자를 스캔합니다.
-  while (
-    !tokenIterResult.done &&
-    isCharType(currentChar, CharType.OperatorAndPunctuator)
-  ) {
-    string += currentChar;
-    tokenIterResult = tokenIter.next();
-    currentChar = tokenIterResult.value;
-  }
-
-  // 유효한 연산자/구두점 토큰을 찾을 때까지 마지막 문자를 제거합니다.
-  // TODO: string이 ()이 된 경우 이터레이터를 앞으로 되돌려야 하는데 타입스크립트는 이터레이터를 앞으로 되돌릴 수 없습니다.
-  // while (string && toKind(string) === Kind.Unknown) {
-  // string = string.slice(0, -1);
-  // current--; (타입스크립트에서는 current의 위치를 직접 조정할 수 없습니다.)
-  // }
-
-  // 스캔된 문자열이 비어있는 경우, 오류를 출력하고 종료합니다.
+  let string = accumulateStringWhileCondition((char) =>
+    isCharType(char, CharType.OperatorAndPunctuator)
+  );
   if (!string) {
-    console.error(currentChar + " 사용할 수 없는 문자입니다.");
-    process.exit(1);
+    throw new Error(tokenIterResult.value + " 사용할 수 없는 문자입니다.");
   }
-
   return new Token(toKind(string), string);
 };
 
-export const getCharType = (char: string) => {
-  if (
-    char === "" ||
-    char === " " ||
-    char === "\\t" ||
-    char === "\\r" ||
-    char === "\\n"
-  ) {
+export const getCharType = (char: string): CharType => {
+  if (["", " ", "\\t", "\\r", "\\n"].includes(char)) {
     return CharType.Whitespace;
   }
-  if (char >= "0" && char <= "9") {
+  if (isCharType(char, CharType.NumberLiteral)) {
     return CharType.NumberLiteral;
   }
   if (char === '"' || char === "'") {
     return CharType.StringLiteral;
   }
-  if (
-    (char >= "a" && char <= "z") ||
-    (char >= "A" && char <= "Z") ||
-    (char >= "0" && char <= "9") ||
-    char === "_"
-  ) {
+  if (isCharType(char, CharType.IdentifierAndKeyword)) {
     return CharType.IdentifierAndKeyword;
   }
-  if (
-    char === "+" ||
-    char === "-" ||
-    char === "*" ||
-    char === "/" ||
-    char === "%" ||
-    char === "=" ||
-    char === "!" ||
-    char === "<" ||
-    char === ">" ||
-    char === "&" ||
-    char === "|" ||
-    char === "^" ||
-    char === "~" ||
-    char === "?" ||
-    char === ":" ||
-    char === "." ||
-    char === "," ||
-    char === ";" ||
-    char === "(" ||
-    char === ")" ||
-    char === "[" ||
-    char === "]" ||
-    char === "{" ||
-    char === "}"
-  ) {
+  if (isCharType(char, CharType.OperatorAndPunctuator)) {
     return CharType.OperatorAndPunctuator;
   }
   return CharType.Unknown;
@@ -185,14 +139,13 @@ let tokenIterResult: IteratorResult<string>;
 
 export const scan = (sourceCode: string): Token[] => {
   const result: Token[] = [];
-
   tokenIter = sourceCode[Symbol.iterator]();
   tokenIterResult = tokenIter.next();
 
   while (!tokenIterResult.done) {
-    const current = tokenIterResult.value;
+    const currentType = getCharType(tokenIterResult.value);
 
-    switch (getCharType(current)) {
+    switch (currentType) {
       case CharType.Whitespace:
         tokenIterResult = tokenIter.next();
         break;
--- src/utils.ts
+++ src/utils.ts
@@ -4,4 +4,12 @@ export const printTokenList = (tokens: Token[]) => {
   tokens.forEach((token) => {
     console.log(token);
   });
+
+  console.log(
+    tokens
+      .map((token) => {
+        return token.string || token.kind;
+      })
+      .join(" ")
+  );
 };
