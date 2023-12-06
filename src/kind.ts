78c910cc9d5a28b83d463d0182464117dceeb070
saengmotmi <ohjtack@gmail.com>
2023-12-06 22:19:20 +0900

    add: stringToKind

--- src/kind.ts
+++ src/kind.ts
@@ -47,3 +47,55 @@ export enum Kind {
   LeftBracket,
   RightBracket,
 }
+
+export const stringToKind = new Map<string, Kind>([
+  ["#unknown", Kind.Unknown],
+  ["#EndOfToken", Kind.EndOfToken],
+  ["null", Kind.NullLiteral],
+  ["true", Kind.TrueLiteral],
+  ["false", Kind.FalseLiteral],
+  ["#Number", Kind.NumberLiteral],
+  ["#String", Kind.StringLiteral],
+  ["#Identifier", Kind.Identifier],
+
+  ["function", Kind.Function],
+  ["return", Kind.Return],
+  ["var", Kind.Variable],
+  ["for", Kind.For],
+  ["break", Kind.Break],
+  ["continue", Kind.Continue],
+  ["if", Kind.If],
+  ["elif", Kind.Elif],
+  ["else", Kind.Else],
+  ["print", Kind.Print],
+  ["println", Kind.PrintLine],
+
+  ["and", Kind.LogicalAnd],
+  ["or", Kind.LogicalOr],
+
+  ["=", Kind.Assignment],
+
+  ["+", Kind.Add],
+  ["-", Kind.Subtract],
+  ["*", Kind.Multiply],
+  ["/", Kind.Divide],
+  ["%", Kind.Modulo],
+
+  ["==", Kind.Equal],
+  ["!=", Kind.NotEqual],
+  ["<", Kind.LessThan],
+  [">", Kind.GreaterThan],
+  ["<=", Kind.LessThanOrEqual],
+  [">=", Kind.GreaterThanOrEqual],
+
+  [",", Kind.Comma],
+  [":", Kind.Colon],
+  [";", Kind.Semicolon],
+  [".", Kind.Dot],
+  ["(", Kind.LeftParen],
+  [")", Kind.RightParen],
+  ["{", Kind.LeftBrace],
+  ["}", Kind.RightBrace],
+  ["[", Kind.LeftBracket],
+  ["]", Kind.RightBracket],
+]);
